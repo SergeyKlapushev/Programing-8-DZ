5ef1e23131c95dc723513123ea4772ee2fd748ac
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-17 12:39:31 +0300

    Taks3 completed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+double [] array = {3, 7, 2.2, 2.7, 78.7};
+double max = Max(array, array.Length);
+double min = Min(array, array.Length);
+Printer(array, max, min);
+
+
+double Max(double [] arr, int len)
+{
+    double max = arr[0];
+    
+    for (int i =0; i< len; i++)
+    {
+        if(max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+
+double Min(double [] arr, int len)
+{
+    double min = arr[0];
+    for (int i =0; i < len; i++)
+    {
+        if(min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+
+void Printer(double [] arr1, double max, double min)
+{
+    for (int i =0; i<arr1.Length; i++)
+    {
+        Console.Write($"{arr1[i]} ");
+    }
+    Console.Write($"-> {max - min}");
+}
